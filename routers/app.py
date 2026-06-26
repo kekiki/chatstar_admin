633e7e0fe581f246ec2e08332ad4ddccaaf6931d
kejie <kekiki@126.com>
2026-06-26 11:45:34 +0800

    bug fixed

--- routers/app.py
+++ routers/app.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request, Depends, Body
+from fastapi import APIRouter, Request, Depends, Body, Query
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session

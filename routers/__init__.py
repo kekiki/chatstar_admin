4137c10f482ec24a330074148fa94b50867bfaa5
kejie <kekiki@126.com>
2026-06-30 15:36:43 +0800

    media list

--- routers/__init__.py
+++ routers/__init__.py
@@ -5,6 +5,7 @@ from .anchor import router as anchor_router
 from .order import router as order_router
 from .app_list import router as app_list_router
 from .app_config import router as app_config_router
+from .media import router as media_router
 
-__all__ = ['auth_router', 'dashboard_router', 'user_router', 'anchor_router', 'order_router', 'app_list_router', 'app_config_router']
+__all__ = ['auth_router', 'dashboard_router', 'user_router', 'anchor_router', 'order_router', 'app_list_router', 'app_config_router', 'media_router']
     

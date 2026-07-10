bcf9149d4123dd9f5fd46efa596d4f08bcb32b1f
kejie <kekiki@126.com>
2026-07-10 18:44:52 +0800

    review config

--- models/stat.py
+++ models/stat.py
@@ -1,10 +1,9 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy import Column, Integer, String, Float
 from database import Base
 
 class DailyStat(Base):
     __tablename__ = "daily_stat"
     id = Column(Integer, primary_key=True)
-    package_name = Column(String(100), ForeignKey("app_list.package_name"), index=True)
     stat_date = Column(String(20)) # yyyy-MM-dd
     new_user = Column(Integer, default=0) # 新增用户
     dau = Column(Integer, default=0) # 日活
